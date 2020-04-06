edd3d846d4ef2436a2ea28bbe0688bd13092e02f
Martin Husbyn <martin.husbyn@flexciton.com>
2020-04-06 13:44:52 +0100

    full example ish

--- coupling_opt_run.py
+++ coupling_opt_run.py
@@ -1,6 +1,8 @@
+import random
+from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import List, Dict
+from typing import List, Dict, Tuple, NewType, Optional
 
 
 @dataclass
@@ -19,8 +21,16 @@ class File:
     pass
 
 
+@dataclass
+class Batch:
+    jobs: List[Job]
+    machine: Optional[Machine] = None
+
+
+@dataclass
 class Schedule:
-    pass
+    batches: List[Batch]
+    metrics: Optional[Dict] = None
 
 
 @dataclass
@@ -53,6 +63,50 @@ class JobPreprocessor:
             job.metadata['next_step_start'] = job.metadata['estimated_next_step_start'] + timedelta(minutes=10)
 
 
+FrobnicationValue = NewType('FrobnicationValue', float)
+
+
+class OptimisationRunner:
+    def run_optimisation(self, opt_data: OptimisationData) -> List[Tuple[Batch, FrobnicationValue]]:
+        batches = []
+        start_time = datetime(2020, 1, 1)
+        cur_end_time = start_time
+        for i in range(0, len(opt_data.jobs), 5):
+            batch = Batch(opt_data.jobs[i:i + 5])
+
+            for j in batch.jobs:
+                j.start = cur_end_time
+                j.end = cur_end_time + timedelta(minutes=20)
+
+            cur_end_time = batch.jobs[0].end
+
+            batches.append((batch, FrobnicationValue(random.random())))
+
+        return batches
+
+
+class ScheduleBuilder:
+    def build_schedule(self, opt_data: OptimisationData, batch_frobnication: List[Tuple[Batch, FrobnicationValue]]):
+        # Frobnication value decides the machine
+        machines = opt_data.machines
+        num_machines = len(machines)
+        for batch, frobnication_value in batch_frobnication:
+            machine_idx = int(frobnication_value * num_machines)
+            batch.machine = machines[machine_idx]
+
+        return Schedule([b for b, _ in batch_frobnication])
+
+
+class ScheduleAnalyser:
+    def analyse_schedule(self, schedule: Schedule):
+        total_post_wait_time = timedelta()
+        for batch in schedule.batches:
+            for job in batch.jobs:
+                total_post_wait_time += job.metadata['next_step_start'] - job.end
+
+        return {'total_post_wait_time': total_post_wait_time}
+
+
 class E2EScheduler:
     def create_schedule(self) -> Schedule:
         data_getter = OptimisationDataGetter()
@@ -63,9 +117,24 @@ class E2EScheduler:
         job_preprocessor.preprocess_jobs(opt_data.jobs)
 
         # Optimisation run
-
-        # Metrics calculator that relies on metadata in preprocessor
+        optimisation_runner = OptimisationRunner()
+        batches = optimisation_runner.run_optimisation(opt_data)
 
         # Schedule creator
+        schedule_builder = ScheduleBuilder()
+        schedule = schedule_builder.build_schedule(opt_data, batches)
 
-        pass
+        # Metrics calculator that relies on metadata in preprocessor
+        schedule_analyser = ScheduleAnalyser()
+        schedule.metrics = schedule_analyser.analyse_schedule(schedule=schedule)
+
+        return schedule
+
+
+
+# Want to cover:
+
+# Law of Demeter
+# Dependency injection
+# Long dict coupling
+# Unnecessary inheritance
